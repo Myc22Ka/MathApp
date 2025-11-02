a9dc0415e8ea6a47c8b64a252a2647d3e398438b
Krzysztof Gach <krzysztof02gach@gmail.com>
2025-11-02 14:31:58 +0100

    fixing bugs

--- backend/src/main/java/pl/myc22ka/mathapp/s3/config/S3Config.java
+++ backend/src/main/java/pl/myc22ka/mathapp/s3/config/S3Config.java
@@ -1,11 +1,9 @@
 package pl.myc22ka.mathapp.s3.config;
 
-import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import pl.myc22ka.mathapp.s3.component.helper.S3Helper;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -20,13 +18,25 @@ import java.net.URI;
 @Configuration
 public class S3Config {
 
+    @Value("${spring.aws.s3.endpoint}")
+    private String endpoint;
+
+    @Value("${spring.minio.username}")
+    private String userName;
+
+    @Value("${spring.minio.password}")
+    private String password;
+
+    @Value("${spring.aws.s3.bucket}")
+    private String bucketName;
+
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .endpointOverride(URI.create("http://localhost:9000"))
+                .endpointOverride(URI.create(endpoint))
                 .credentialsProvider(
                         StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create("minioadmin", "minioadmin")
+                                AwsBasicCredentials.create(userName, password)
                         )
                 )
                 .region(Region.US_EAST_1)
@@ -39,7 +49,7 @@ public class S3Config {
     }
 
     @Bean
-    public String initializeBucket(@NotNull S3Client s3Client, @Value("${spring.aws.s3.bucket}") String bucketName) {
+    public String initializeBucket(@NotNull S3Client s3Client) {
         try {
             s3Client.headBucket(HeadBucketRequest.builder().bucket(bucketName).build());
         } catch (NoSuchBucketException e) {
